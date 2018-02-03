0a1f9ea04520349ea5a152b10cee128def2546ac
kamil <szamocki16@gmail.com>
2018-02-03 16:52:32 +0100

    kamsza - drugi commit

--- funkcja1.cpp
+++ funkcja1.cpp
@@ -1,1 +1,4 @@
-
+int funkcja1(int a,int b)
+{
+return a+b;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <funkcja1.cpp>
 
 using namespace std;
 
 int main()
 {
+cout<<"Podaj a i b"<<endl;
+cin>>a;
+cin>>b;
+
+cout<<endl;
+funkcja(a,b);
 
 
 return;
